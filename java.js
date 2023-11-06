e63ee94298eb9bfb6a43095db61eec0a04f24f5d
Justin_Flores <225jflores@frhsd.com>
2023-11-06 11:50:27 -0500

    Minor Impr.

--- java.js
+++ java.js
@@ -6,4 +6,12 @@ function myFunction(x) {
     document.getElementById("demo").innerHTML = "Hello World";
   }
 
-  
+  // Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
